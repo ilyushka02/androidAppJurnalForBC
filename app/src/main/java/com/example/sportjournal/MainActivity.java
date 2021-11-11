c3491f5014127cabb528b04a04c609ccb39a94a9
Ilya Nasurllaev <ilyaevgenievi4@mail.ru>
2021-11-11 16:31:20 +0300

    update signup page

--- app/src/main/java/com/example/sportjournal/MainActivity.java
+++ app/src/main/java/com/example/sportjournal/MainActivity.java
@@ -2,14 +2,45 @@ package com.example.sportjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private  TextView textView_createAcc;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        addListener();
     }
+
+    public  void  addListener(){
+        textView_createAcc =(TextView) findViewById(R.id.textCreateNewAccount);
+        btn = (Button)  findViewById(R.id.signin_btn);
+        btn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("android.intent.action.Signup");
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        textView_createAcc.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("android.intent.action.Signup");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
 }
