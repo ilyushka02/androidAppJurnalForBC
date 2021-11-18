9f410e89be84f7e8c52b50ae35c9d1462abc281b
Ilya Nasurllaev <ilyaevgenievi4@mail.ru>
2021-11-18 14:20:18 +0300

    rewrite function for swap page

--- app/src/main/java/com/example/sportjournal/MainActivity.java
+++ app/src/main/java/com/example/sportjournal/MainActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private  TextView textView_createAcc;
     private Button btn;
     @Override
@@ -16,31 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        addListener();
+        //Находим элементы и устанавливаем на них слушатель нажатий
+        Button btn_back = findViewById(R.id.signin_btn);
+        btn_back.setOnClickListener(this);
+        TextView textView_createAcc = findViewById(R.id.textCreateNewAccount);
+        textView_createAcc.setOnClickListener(this);
     }
 
-    public  void  addListener(){
-        textView_createAcc =(TextView) findViewById(R.id.textCreateNewAccount);
-        btn = (Button)  findViewById(R.id.signin_btn);
-        btn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("android.intent.action.Signup");
-                        startActivity(intent);
-                    }
-                }
-        );
-
-        textView_createAcc.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent("android.intent.action.Signup");
-                        startActivity(intent);
-                    }
-                }
-        );
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signin_btn:
+                Log.d(MainActivity.class.getSimpleName() ,"Sign in btn detected ------------- click click click ");
+                break;
+            case R.id.textCreateNewAccount:
+                openSignup();
+                break;
+        }
     }
 
+    private void openSignup() {
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
+    }
 }
